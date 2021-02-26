d856f69756ac170388944c3a9e96a11bd0d60bf0
varun-parthasarathy <varunparthasarathy7@gmail.com>
2021-02-26 12:53:54 +0530

    Fix minor bug in LFW dataset generation

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -214,7 +214,8 @@ def get_LFW_dataset(data_path, image_size, batch_size, crop_size, cache='', trai
         return tf.argmax(parts[-2] == CLASS_NAMES) + train_classes
 
     def decode_img(img):
-        img = tf.io.decode_image(img, channels=3)
+        #img = tf.io.decode_image(img, channels=3, expand_animations=False)
+        img = tf.io.decode_png(img)
         if use_mixed_precision is True:
             if use_tpu is True:
                 img = tf.cast(img, tf.bfloat16)
