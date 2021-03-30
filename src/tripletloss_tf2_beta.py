aadbd948c7a9e5f96b9141b8692407ee16bccd4e
varun-parthasarathy <varunparthasarathy7@gmail.com>
2021-03-30 12:18:40 +0530

    Create new log directory for each run based on current time

--- src/tripletloss_tf2_beta.py
+++ src/tripletloss_tf2_beta.py
@@ -283,7 +283,8 @@ def train_model(data_path, batch_size, image_size, crop_size, lr_schedule_name, 
     else:
         decay_margin_callback = None
 
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs", update_freq=100,
+    log_dir = './logs/log_' + datetime.now().strftime("%Y%m%d_%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq=100,
                                                           write_graph=False)
 
     triplet_loss_metrics = TripletLossMetrics(test_images, embedding_size)
