2495fe8a7eb1de07352a95e1ac68722a33b94741
varun-parthasarathy <varunparthasarathy7@gmail.com>
2021-10-27 10:18:51 +0530

    Restore support for cosine decay with warm restarts LR policy

--- src/tripletloss_tf2_beta.py
+++ src/tripletloss_tf2_beta.py
@@ -176,12 +176,12 @@ def get_learning_rate_schedule(schedule_name, image_count, batch_size, learning_
         lr = learning_rate
     else:
         pass
-    '''elif schedule_name == 'cosine_restart':
+    elif schedule_name == 'cosine_restart':
         lr = tf.keras.optimizers.schedules.CosineDecayRestarts(initial_learning_rate=max_lr,
                                                                first_decay_steps=step_size,
                                                                t_mul=1.0, # Can be 2.0 as well, but 1.0 works just fine
-                                                               m_mul=0.90, # Decay the starting lr for each restart
-                                                               alpha=learning_rate)'''
+                                                               m_mul=0.80, # Decay the starting lr for each restart
+                                                               alpha=learning_rate)
 
     assert lr is not None, '[ERROR] The learning rate schedule is not specified correctly'
 
